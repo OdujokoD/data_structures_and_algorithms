88ae335d544ae01d1203c05f08cf79d914220fd7
OdujokoD <odujoko.dml30@gmail.com>
2018-06-24 14:48:07 +0100

    Implement ArrayList data structure and a notebook of the explanation

---
+++ test/arraylist_testcases.py
@@ -0,0 +1,51 @@
+import unittest
+import arraylist as a
+
+
+class MyTest(unittest.TestCase):
+
+    # Test make array method
+    def test1(self):
+        array = a.ArrayList()
+        length = 0
+
+        self.assertEqual(length, array.length())
+
+    def test2(self):
+        array = a.ArrayList()
+        for i in range(5):
+            array.add(i)
+        length = 5
+        self.assertEqual(length, array.length())
+
+    def test3(self):
+        array = a.ArrayList()
+        length = 10
+        for i in range(length):
+            array.add(i)
+
+        self.assertEqual(length, array.length())
+
+    def test4(self):
+        array = a.ArrayList()
+        for i in range(5):
+            array.add(i)
+        self.assertEqual(array.get(4), 4)
+
+    def test5(self):
+        array = a.ArrayList()
+        for i in range(5):
+            array.add(i)
+
+        self.assertEqual(array.get(0), 0)
+
+    def test6(self):
+        array = a.ArrayList()
+        for i in range(5):
+            array.add(i)
+
+        self.assertRaises(array.get(10), IndexError('Index out of bound'))
+
+
+if __name__ == '__main__':
+    unittest.main()
